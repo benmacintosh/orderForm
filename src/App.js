63812dd5c470571d7bda86df71a52eac19c4d655
benmacintosh <ben.macintosh0@gmailcom>
2018-10-14 16:52:30 +1100

    update

--- src/App.js
+++ src/App.js
@@ -19,6 +19,106 @@ import {withRouter} from 'react-router-dom'
 
 
 
+
+
+class AddressForm extends Component {
+
+    constructor(props) {
+        super(props);   
+        this.state = {value: "origin"+this.props.id, method: "WALKING", methods: ["TRANSIT","WALKING<","DRIVING","BICYCLING"], id: this.props.id, latlng: ["",""]};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.onClickAuto1 = this.onClickAuto1.bind(this);
+        this.onClickAuto2 = this.onClickAuto2.bind(this);
+        this.onClickAuto3 = this.onClickAuto3.bind(this);
+        this.onClickAuto4=this.onClickAuto4.bind(this);
+        this.onClick= this.onClick.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({value: event.target.value});
+        var autocompleteapi = new window.google.maps.places.Autocomplete(document.getElementById(this.state.id));
+        var latlng = ["",""]
+        var tempsetstate = this.setState.bind(this)
+
+        //make so doesnt call after EVERY KEYSTROKE  BTU AFTER SOME TIME delay
+
+        //CHECK NEARBY USER
+
+        window.google.maps.event.addListener(autocompleteapi, 'place_changed', function () {
+            var place = autocompleteapi.getPlace()
+            var address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+
+            latlng = [place.geometry.location.lat(),place.geometry.location.lng()]
+            tempsetstate({value: address, latlng: latlng})
+        })
+    }
+
+    onClickAuto1(event){
+        this.setState({methods:["TRANSIT","WALKING","DRIVING","BICYCLING"]})
+        this.setState({method:this.state.methods[0]})
+        this.setState({methods:["TRANSIT<","WALKING","DRIVING", "BICYCLING"]})
+
+    }
+    onClickAuto2(event){
+        this.setState({methods:["TRANSIT","WALKING","DRIVING","BICYCLING"]})
+        this.setState({method: this.state.methods[1]})
+        this.setState({methods:["TRANSIT","WALKING","DRIVING","BICYCLING"]})
+
+    }
+    onClickAuto3(event){
+        this.setState({methods:["TRANSIT","WALKING","DRIVING","BICYCLING"]})
+        this.setState({method: this.state.methods[2]})
+        this.setState({methods:["TRANSIT","WALKING","DRIVING<","BICYCLING"]})
+
+    }
+    onClickAuto4(event){
+        this.setState({methods:["TRANSIT","WALKING","DRIVING","BICYCLING"]})
+        this.setState({method: this.state.methods[3]})
+        this.setState({methods:["TRANSIT","WALKING","DRIVING","BICYCLING<"]})    
+    }
+    componentDidUpdate(props){
+        console.log("address form did update")
+        console.log(this.state)
+        console.log(this.props)
+        this.props.callFromKids(this.state);
+    }
+
+    onClick(event){
+        this.setState({value: ""})
+    }
+
+    render(){
+        return(
+            <div>
+
+            <input type = "text" id={this.state.id} value = {this.state.value} onChange = {this.handleChange} onClick={this.onClick} />
+
+            <a onClick={this.onClickAuto1} style={{color:'blue', cursor: 'pointer', textDecorationLine: 'underline'}}>{this.state.methods[0]}</a>
+            <a>, </a>
+            <a onClick={this.onClickAuto2} style={{color:'blue', cursor: 'pointer', textDecorationLine: 'underline'}}>{this.state.methods[1]}</a>
+            <a>, </a>
+            <a onClick={this.onClickAuto3} style={{color:'blue', cursor: 'pointer', textDecorationLine: 'underline'}}>{this.state.methods[2]}</a>
+            <a>, </a>
+            <a onClick={this.onClickAuto4} style={{color:'blue', cursor: 'pointer', textDecorationLine: 'underline'}}>{this.state.methods[3]}</a>
+
+            </div>
+            );
+    }
+}
+
+
+
+
+
+
+
+
+
 class Bid extends Component{
 
 
@@ -208,11 +308,13 @@ class Bid extends Component{
 class App extends Component {
   render() {
     return (
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+      <div style={{display: 'flex',  justifyContent:'left', alignItems:'left', height: '100vh'}}>
+
+      <AddressForm />
 
         <div>songs for music macintosh archival documents (sonnets for cnn)</div>
         <p>
-        1. I love watching the 2. evening parisian train 3. icecube paperweight 4. panther bear sticker 5. FLLINGLIKE66 6. harbour focus 7. mad rush (to see you) 8. small britney4 9. summer learn 10. why sit 11. my best mind 12. he was a friend of mine 13. henri can sit 14. pre syria 15. smile its miles 16. ok freestyle 17. tech III 18. comfortable, ALRIGHT 19. thanking you 20. patrolling from the sky 21. forgiven 4 reference 22. join the dance 23. mix2 24. after show lone 25. all fits 26. wishes both his eyes were glass 27. magnet research 28. the bad 29. waking jet-1 30. ice storm 31. fly to class 32. 90s info 33. tunnel3 34. intermission 35. supergirl 36. walking sideways 37. WHOS RESIDENCY 38. underwater sandstone 39. take out 40 40. not so well 41. talk to your friends 42. no heart hotel 43. daytime mind 44. blue dream fuck 45. in this bin 46. be nice 47. money on 48. new sadness 49. sample this 50. Emerson 974 51. restyourlove 52. digital water interlude 53. take your time 54. welldone 55. bea1 56. bwv106 57. Free melodys 58. globalised trees 59. sony freestyle 60. beautiful stairs 61. pop goes the weasal across the universe 62. walk5 63. WALK UP 64. skys 65. battle time 66. nocturne 67. small hand man sun 68. roville let go 69. now and again 70. greatnss
+        1. I love watching the 2. evening parisian train 3. icecube paperweight 4. panther bear sticker 5. FLLINGLIKE66 6. harbour focus 7. mad rush (to see you) 8. small britney4 9. summer learn 10. why sit 11. my best mind 12. he was a friend of mine 13. henri can sit 14. pre syria 15. smile its miles 16. ok freestyle 17. tech III 18. comfortable, ALRIGHT 19. thanking you 20. patrolling from the sky 21. forgiven 4 reference 22. join the dance 23. mix2 24. after show lone 25. all fits 26. wishes both his eyes were glass 27. magnet research 28. the bad 29. waking jet-1 30. ice storm 31. fly to class 32. 90s info 33. tunnel3 34. intermission 35. supergirl 36. walking sideways 37. WHOS RESIDENCY 38. underwater sandstone 39. take out 40 40. not so well 41. talk to your friends 42. no heart hotel 43. daytime mind 44. blue dream fuck 45. in this bin 46. be nice 47. money on 48. new sadness 49. sample this 50. Emerson 974 51. restyourlove 52. digital water interlude 53. take your time 54. welldone 55. bea1 56. bwv106 57. Free melodys 58. globalised trees 59. sony freestyle 60. beautiful stairs 61. pop goes the weasal across the universe 62. walk5 63. WALK UP 64. skys 65. battle time 66. nocturne 67. small hand man sun 67.6 hand stable 68. roville let go 69. now and again 70. greatnss
         </p>ass
 
         <p>
